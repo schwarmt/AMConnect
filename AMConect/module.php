fbb3501f1d79937c69b918111e222cdf0f101b53
schwarmt <martin@blacktec.de>
2020-02-17 00:02:33 +0100

    first commit

--- AMConect/module.php
+++ AMConect/module.php
@@ -262,6 +262,7 @@ class AMConnect extends IPSModule {
                 $Variablen_ID = $this->GetIDForIdent($key);
                 if(isset($Variablen_ID)) {
                     $id=$value[0];
+                    $factor=$value[1];
                     if(array_key_exists($id,$json)){
                         $am_value = $json->$id;
                         $Variable_Daten = IPS_GetVariable($Variablen_ID);
@@ -273,10 +274,10 @@ class AMConnect extends IPSModule {
                                 SetValueBoolean($Variablen_ID, $am_value);
                                 break;
                             case 1:
-                                SetValueInteger($Variablen_ID, $am_value);
+                                SetValueInteger($Variablen_ID, $am_value*$factor);
                                 break;
                             case 2:
-                                SetValueFloat($Variablen_ID, $am_value);
+                                SetValueFloat($Variablen_ID, $am_value*$factor);
                                 break;
                             case 3:
                                 SetValueString($Variablen_ID, $am_value);
