bd60b841d6cc53b31ffedeefafb34a12a04db090
schwarmt <martin@blacktec.de>
2020-04-11 22:20:01 +0200

    Timestamps ergänzt

--- AMConect/module.php
+++ AMConect/module.php
@@ -290,12 +290,12 @@ class AMConnect extends IPSModule {
             // Update-Timestamp Nachverarbeitung
             $unixString = GetValueString($this->GetIDForIdent('LastUpdate'));
             IPS_LogMessage($_IPS['SELF'], ">unixString: $unixString");
-            $dt= DateTime::createFromFormat("YYYYMMDDHHIISS",$unixString );
+            $dt= DateTime::createFromFormat("YYYYmmddHHiiss",$unixString );
             IPS_LogMessage($_IPS['SELF'], ">dt: $dt");
             $unixTS =  $dt->getTimestamp();
             IPS_LogMessage($_IPS['SELF'], " >unixTS $unixTS");
             SetValueInteger($this->GetIDForIdent("LastUpdateTS"), $unixTS);
-            $unixTS =  DateTime::createFromFormat("YYYYMMDDHHIISS", GetValueString($this->GetIDForIdent('LastUpdateStatus')))->getTimestamp();
+            $unixTS =  DateTime::createFromFormat("YYYYmmddHHiiss", GetValueString($this->GetIDForIdent('LastUpdateStatus')))->getTimestamp();
             SetValueInteger($this->GetIDForIdent("LastUpdateStatusTS"), $unixTS);
         }
         return true;
