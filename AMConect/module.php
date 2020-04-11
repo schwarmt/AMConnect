acd4a3d0c748ce984331be7bf55bab0bb37346bb
schwarmt <martin@blacktec.de>
2020-04-11 23:13:17 +0200

    Timestamps ergänzt

--- AMConect/module.php
+++ AMConect/module.php
@@ -181,7 +181,7 @@ class AMConnect extends IPSModule {
                 if($mode_text != ""){
                     IPS_LogMessage($_IPS['SELF'], "found mode:".$mode_text);
                     SetValueString($this->GetIDForIdent("ModeText"), $mode_text);
-                    //SendMode();
+                    SendMode();
                 }
                 break;
             case 'Active':
